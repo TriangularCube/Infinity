17b53a9f2b9f2085d6edae18d0ae8075864d1b09
Michael Liu <Michael.Liu0@gmail.com>
2014-09-13 16:41:12 -0700

    Added rotation to CameraControls' focusing logic.

--- Assets/Scripts/Misc/CameraControls.cs
+++ Assets/Scripts/Misc/CameraControls.cs
@@ -7,6 +7,7 @@ public class CameraControls : MonoBehaviour {
 	private Transform targetCameraPoint{ get{ return target.cameraPoint; } }
 
 	public float camTransferSpeed = 600f;
+	public float camTransferRotation = 40f;
 
 	public void SetTarget( Transform parent, ShipControl controller ){
 		transform.parent = parent;
@@ -17,15 +18,20 @@ public class CameraControls : MonoBehaviour {
 	}
 
 	void Update(){
-		if (target) {
-			transform.position = Vector3.MoveTowards (transform.position, targetCameraPoint.position, camTransferSpeed * Time.deltaTime);
+		//If this script is enabled, but we don't have a target, turn ourselves off.
+		if (!target) {
+			enabled = false;
+			return;
+		}
 
-			if (Vector3.Distance (transform.position, targetCameraPoint.position) < 1f) {
-				target.TransferControl (gameObject);
+		transform.position = Vector3.MoveTowards (transform.position, targetCameraPoint.position, camTransferSpeed * Time.deltaTime);
+		transform.rotation = Quaternion.RotateTowards( transform.rotation, targetCameraPoint.rotation, camTransferRotation * Time.deltaTime);
 
-				target = null;
-				enabled = false;
-			}
+		if (Vector3.Distance (transform.position, targetCameraPoint.position) < 1f) {
+			target.TransferControl (gameObject);
+
+			target = null;
+			enabled = false;
 		}
 	}
 }
