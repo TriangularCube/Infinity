9d3a57f2c1aa32404269d92fcdb17dd572a23c54
Michael Liu <Michael.Liu0@gmail.com>
2014-09-13 17:25:55 -0700

    Implemented Dock.

---
+++ Assets/Scripts/Ship/Dock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dock : MonoBehaviour {
+
+	public Carrier carrier;
+
+	void OnTriggerEnter( Collider other ){
+		if (other.gameObject.GetComponent<Terminal> () != null) {
+			carrier.Dock( other.gameObject );
+		}
+	}
+}
