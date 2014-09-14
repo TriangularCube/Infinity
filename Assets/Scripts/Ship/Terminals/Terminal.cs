ca13275a8cfb86ae4c54fa1b5f4428e8b2bb9ab5
Michael Liu <Michael.Liu0@gmail.com>
2014-09-13 18:52:52 -0700

    Added AssignDefault to Terminal, which turns out all Terminals will use (since they all just assign to Pilot anyway). Implemented Mech class, but currently has no extra functionality on top of Terminal.

--- Assets/Scripts/Ship/Terminals/Terminal.cs
+++ Assets/Scripts/Ship/Terminals/Terminal.cs
@@ -3,11 +3,28 @@ using System.Collections;
 
 public abstract class Terminal : Ship {
 
+	public ShipControl control;
 	public TNet.Player pilot{ get; set; }
 
 	public override bool ContainsPlayer (TNet.Player check)
 	{
 		return pilot == check;
+	}
+
+	//TODO
+	public override void AssignDefault (TNet.Player pilot)
+	{
+		if (TNManager.isHosting) {
+			tno.Send( "AssignDefault", TNet.Target.Others, pilot );
+
+			PlayersManager.Instance.ApplyFocusChange ( pilot, tno.uid, "Pilot" );
+		}
+
+		if (pilot == TNManager.player) {
+			playerCameraControl.SetTarget( transform, control );
+		}
+
+		this.pilot = pilot;
 	}
 
 }
