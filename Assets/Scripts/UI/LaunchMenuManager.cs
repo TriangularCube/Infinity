655fa817c15cc82775483769309e00e784bf7c61
Michael Liu <Michael.Liu0@gmail.com>
2014-09-14 22:54:06 -0700

    Implemented some preliminary functions.
    NOTE: Launching a Terminal needs more work. Reverse lookup in a Dictionary is bad practice.

--- Assets/Scripts/UI/LaunchMenuManager.cs
+++ Assets/Scripts/UI/LaunchMenuManager.cs
@@ -8,13 +8,58 @@ public class LaunchMenuManager : MonoBehaviour {
 	//Editor references
 	public GameObject shipSelectButton;
 	public UIGrid grid;
+	public GameObject NoShipsDocked;
 
 	//Our list of docked Terminals
-	private List<GameObject> dockedTerminals;
+	private Dictionary< GameObject, GameObject > dockedTerminals;
 
-	public void Test(){
-		NGUITools.AddChild (grid.gameObject, shipSelectButton);
+	void Awake(){
+		dockedTerminals = new Dictionary<GameObject, GameObject>(1);
+	}
+
+	//When we change ships
+	public void Reset(){
+		dockedTerminals.Clear ();
+		NoShipsDocked.SetActive (true);
+	}
+
+	public void PopulateList( Carrier carrier ){
+
+		//What should be a redundent check
+		if (dockedTerminals.Count > 0)
+						throw new UnityException ("Launch Menu's list is being populated while it's still filled");
+
+		if (carrier.dockedTerminals.Count > 0) {
+		//Fill the grid with a button for each Terminal in the carrier
+			foreach (GameObject terminal in carrier.dockedTerminals) {
+				dockedTerminals.Add (NGUITools.AddChild (grid.gameObject, shipSelectButton), terminal);
+			}
+			grid.Reposition ();
+		} else {
+			NoShipsDocked.SetActive( true );
+		}
+	}
+
+	public void TerminalDocked( GameObject terminal ){
+
+		NoShipsDocked.SetActive (false);
+
+		dockedTerminals.Add (NGUITools.AddChild (grid.gameObject, shipSelectButton), terminal);
 		grid.Reposition ();
+
+	}
+
+	public void TerminalLaunched( GameObject terminal ){
+
+		//HACK This probably won't happen...?
+
+		//If there's no values in the list of terminals
+		if (dockedTerminals == null || dockedTerminals.Count == 0) {
+			//Turn the label that says "No Terminals docked"
+			NoShipsDocked.SetActive (true);
+		} else {
+			NoShipsDocked.SetActive (false);
+		}
 	}
 
 	//HACK
