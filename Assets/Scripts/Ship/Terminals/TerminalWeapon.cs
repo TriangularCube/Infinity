880553614deaac6c50269e0fa0f55ea50210ee92
Michael Liu <Michael.Liu0@gmail.com>
2014-10-08 22:50:02 -0700

    Implemented a Terminal Weapon interface. Registers itself with Firecontrol on Awake. See #14.

---
+++ Assets/Scripts/Ship/Terminals/TerminalWeapon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerminalWeapon : MonoBehaviour {
+
+	void Awake(){
+		transform.root.GetComponent<TerminalFireControl> ().RegisterWeapon (this);
+	}
+
+	[SerializeField]
+	private GameObject Ammo;
+
+	[SerializeField]
+	private float coolDown = 0.1f;
+	private bool isOnCooldown = false;
+
+	public void Fire(){
+		//TODO Fire stuff
+
+		if (!isOnCooldown) {
+			Instantiate( Ammo, transform.position, transform.rotation );
+
+			StartCoroutine( Cooldown() );
+		}
+	}
+
+	IEnumerator Cooldown(){
+		isOnCooldown = true;
+		yield return new WaitForSeconds( coolDown );
+		isOnCooldown = false;
+	}
+
+}
