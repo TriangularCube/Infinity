828d50874090215b9eddac742201b392fdd602c4
Michael Liu <Michael.Liu0@gmail.com>
2014-10-08 22:47:00 -0700

    Made prefab for Auto Cannon's tracer round. Implemented generic script to move the round and destroy after a set time.

---
+++ Assets/Scripts/Ship/Terminals/Interceptor/InterceptorAutoCannonRound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterceptorAutoCannonRound : MonoBehaviour {
+
+	[SerializeField]
+	float speed = 10f;
+
+	void Awake(){
+		Invoke ("Expired", 3f);
+	}
+
+	void Update(){
+		//TODO Do a spherecast here
+
+		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+	}
+
+	void Expired(){
+		Destroy (gameObject);
+	}
+
+}
