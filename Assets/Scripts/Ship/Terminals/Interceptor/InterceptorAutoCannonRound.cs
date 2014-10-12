81604f8413ea1ac4fd468d5634095b20de051575
Michael Liu <Michael.Liu0@gmail.com>
2014-10-11 21:39:26 -0700

    Moved autocannon transformation to FixedUpdate. Now also keeps the velocity of the ship that fired it, so it aligns with the ship.

--- Assets/Scripts/Ship/Terminals/Interceptor/InterceptorAutoCannonRound.cs
+++ Assets/Scripts/Ship/Terminals/Interceptor/InterceptorAutoCannonRound.cs
@@ -6,14 +6,20 @@ public class InterceptorAutoCannonRound : MonoBehaviour {
 	[SerializeField]
 	float speed = 150f;
 
+	private Vector3 firingVector;
+
 	void Awake(){
 		Invoke ("Expired", 3f);
 	}
 
-	void Update(){
+	public void PassFiringVector( Vector3 vector ){
+		firingVector = transform.InverseTransformDirection( vector );
+	}
+
+	void FixedUpdate(){
 		//TODO Do a spherecast here
 
-		transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		transform.Translate ( ( ( Vector3.forward * speed ) + firingVector ) * Time.deltaTime);
 	}
 
 	void Expired(){
--- Assets/Scripts/Ship/Terminals/TerminalWeapon.cs
+++ Assets/Scripts/Ship/Terminals/TerminalWeapon.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class TerminalWeapon : MonoBehaviour {
 
+	//Cached rigidbody
+	Rigidbody rootRigidbody;
+
 	void Awake(){
 		transform.root.GetComponent<TerminalFireControl> ().RegisterWeapon (this);
+		rootRigidbody = transform.root.rigidbody;
 	}
 
 	[SerializeField]
@@ -14,12 +18,14 @@ public class TerminalWeapon : MonoBehaviour {
 	private float coolDown = 0.1f;
 	private bool isOnCooldown = false;
 
+
+
 	public void Fire(){
 		//TODO Fire stuff
 
 		if (!isOnCooldown) {
-			Instantiate( Ammo, transform.position, transform.rotation );
-
+			GameObject obj = (GameObject) Instantiate( Ammo, transform.position, transform.rotation );
+			obj.SendMessage( "PassFiringVector", rootRigidbody.velocity, SendMessageOptions.RequireReceiver );
 			StartCoroutine( Cooldown() );
 		}
 	}
