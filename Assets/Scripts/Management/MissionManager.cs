1bbdb35ec142c4b471428e085725809746f0bbc2
Michael Liu <Michael.Liu0@gmail.com>
2014-11-14 22:05:02 -0800

    Mission Manager implemented. At the moment it handles start of game assignment. Eventually it will handle mission controls (e.g. AI initialization, player starting ship assignment, banter queue, etc.)

---
+++ Assets/Scripts/Management/MissionManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissionManager : MonoBehaviour {
+
+	[SerializeField]
+	Ship startingShip;
+
+	// Use this for initialization
+	void Start () {
+
+		startingShip.AssignDefault( TNManager.player );
+
+	}
+
+	void Update(){
+
+		//DEBUG
+		if( Input.GetKeyDown( KeyCode.LeftControl ) ){
+			Screen.lockCursor = !Screen.lockCursor;
+		}
+
+	}
+
+}
