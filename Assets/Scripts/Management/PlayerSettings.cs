38b0d17f57be85625f8035e63baf4ec9b405eeff
Michael Liu <Michael.Liu0@gmail.com>
2014-11-14 22:08:10 -0800

    Player Profile class. Will load profiles and settings on game start.

---
+++ Assets/Scripts/Management/PlayerSettings.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum InterceptorLookMode{ Locked, Free };
+
+public class Profile{
+	
+	InterceptorLookMode interceptorLookMode = InterceptorLookMode.Free;
+	
+	public InterceptorLookMode GetInterceptorLookMode(){
+		return interceptorLookMode;
+	}
+	
+}
+
+public class PlayerSettings {
+
+	#region Profile Settings
+	public static InterceptorLookMode GetInterceptorLookMode(){
+		return currentProfile.GetInterceptorLookMode ();
+	}
+	#endregion
+
+	static Profile currentProfile;
+
+	static PlayerSettings(){
+		//TODO Load a list of player profiles
+		currentProfile = new Profile ();
+	}
+
+	public static void SelectProfile(){
+
+	}
+}
