251ff0242e61af09207b2f5a3eb6e2cf1407f9f4
Michael Liu <Michael.Liu0@gmail.com>
2015-05-17 22:14:22 -0700

    Updated some turret tracking algorithms.

--- Assets/Scripts/Mission/Non-Player/Enemy/Ships/TurretTracking.cs
+++ Assets/Scripts/Mission/Non-Player/Enemy/Ships/TurretTracking.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class TurretTracking : MonoBehaviour {
 
     public GameObject go;
+    public float rotateSpeed = 2f;
 
     void Update() {
-        transform.localRotation = Quaternion.LookRotation( transform.root.InverseTransformDirection( go.transform.position - transform.position ) );
+        Quaternion newQuat = Quaternion.LookRotation( transform.root.InverseTransformDirection( go.transform.position - transform.position ) );
+        if( newQuat.eulerAngles.x < 270f )
+            newQuat = Quaternion.Euler( 0f, newQuat.eulerAngles.y, newQuat.eulerAngles.z );
+        else
+            newQuat = Quaternion.Euler( Mathf.Clamp( newQuat.eulerAngles.x, 270f, 359f ), newQuat.eulerAngles.y, newQuat.eulerAngles.z );
+        transform.localRotation = Quaternion.RotateTowards( transform.localRotation, newQuat, rotateSpeed );
     }
 
 }
